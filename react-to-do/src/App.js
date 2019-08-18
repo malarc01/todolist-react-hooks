7e9ad1a16f5247586ae4a15e144a95c8db76283d
malarc01 <45378877+malarc01@users.noreply.github.com>
2019-08-18 15:49:47 -0400

    added completeTodo & removeTodo const

--- react-to-do/src/App.js
+++ react-to-do/src/App.js
@@ -1,7 +1,15 @@
 import React, { useState } from 'react';
 import './App.css';
-function Todo({ todo, index }) {
-	return <div className="todo">{todo.text}</div>;
+function Todo({ todo, index, completeTodo, removeTodo }) {
+	return (
+		<div style={{ textDecoration: todo.isCompleted ? 'line-through' : '' }} className="todo">
+			{todo.text}
+			<div>
+				<button onClick={() => completeTodo(index)}>Complete</button>
+				<button onClick={() => removeTodo(index)}>x</button>
+			</div>
+		</div>
+	);
 }
 function TodoForm({ addTodo }) {
 	const [ value, setValue ] = useState('');
@@ -47,10 +55,22 @@ function App() {
 		setTodos(newTodos);
 	};
 
+	const completeTodo = (index) => {
+		const newTodos = [ ...todos ];
+		newTodos[index].isCompleted = true;
+		setTodos(newTodos);
+	};
+	const removeTodo = (index) => {
+		const newTodos = [ ...todos ];
+		newTodos.splice(index, 1);
+		setTodos(newTodos);
+	};
 	return (
 		<div className="app">
 			<div className="todo-list">
-				{todos.map((todo, index) => <Todo key={index} index={index} todo={todo} />)}
+				{todos.map((todo, index) => (
+					<Todo key={index} index={index} todo={todo} completeTodo={completeTodo} removeTodo={removeTodo} />
+				))}
 				<TodoForm addTodo={addTodo} />
 			</div>
 		</div>
