8ea57477b09960244a20785caf7a7726adf6b965
malarc01 <45378877+malarc01@users.noreply.github.com>
2019-08-18 15:35:41 -0400

    added TodoForm & input syling

--- react-to-do/src/App.js
+++ react-to-do/src/App.js
@@ -3,6 +3,29 @@ import './App.css';
 function Todo({ todo, index }) {
 	return <div className="todo">{todo.text}</div>;
 }
+function TodoForm({ addTodo }) {
+	const [ value, setValue ] = useState('');
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if (!value) return;
+		addTodo(value);
+		setValue('');
+	};
+
+	return (
+		<form onSubmit={handleSubmit}>
+			<input
+				type="text"
+				className="input"
+				value={value}
+				placeholder="Add Todo..."
+				onChange={(e) => setValue(e.target.value)}
+			/>
+		</form>
+	);
+}
+
 function App() {
 	const [ todos, setTodos ] = useState([
 		{
@@ -10,7 +33,7 @@ function App() {
 			isCompleted: false
 		},
 		{
-			text: 'Meet friend for lunch',
+			text: 'Eat lunch',
 			isCompleted: false
 		},
 		{
@@ -19,10 +42,16 @@ function App() {
 		}
 	]);
 
+	const addTodo = (text) => {
+		const newTodos = [ ...todos, { text } ];
+		setTodos(newTodos);
+	};
+
 	return (
 		<div className="app">
 			<div className="todo-list">
 				{todos.map((todo, index) => <Todo key={index} index={index} todo={todo} />)}
+				<TodoForm addTodo={addTodo} />
 			</div>
 		</div>
 	);
