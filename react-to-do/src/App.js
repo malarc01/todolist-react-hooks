699755a83749713a0a1e9e610d323c3d4c91df1a
malarc01 <45378877+malarc01@users.noreply.github.com>
2019-08-18 15:17:48 -0400

    added Hooks & some App.css styling

---
+++ react-to-do/src/App.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import './App.css';
+function Todo({ todo, index }) {
+	return <div className="todo">{todo.text}</div>;
+}
+function App() {
+	const [ todos, setTodos ] = useState([
+		{
+			text: 'Learn Hooks',
+			isCompleted: false
+		},
+		{
+			text: 'Meet friend for lunch',
+			isCompleted: false
+		},
+		{
+			text: 'Build todolist ',
+			isCompleted: false
+		}
+	]);
+
+	return (
+		<div className="app">
+			<div className="todo-list">
+				{todos.map((todo, index) => <Todo key={index} index={index} todo={todo} />)}
+			</div>
+		</div>
+	);
+}
+
+export default App;
